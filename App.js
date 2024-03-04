1f8292cc7f909860937385a1773b27bbe31f62c0
Younes Bieche <younes.bieche@gmail.com>
2024-03-04 11:10:28 +0800

    init

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { CameraView } from "expo-camera/next";
 
 export default function App() {
+  const cameraRef = React.useRef();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CameraView
+      facing="front"
+      ref={cameraRef}
+      style={StyleSheet.absoluteFill}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
